628b2cb7e6dd908768a24d479126802fcbd7173f
rorusage <rorusage@gmail.com>
2016-12-15 16:58:00 +0800

    Build create, edit, show, destroy actions

--- app/controllers/reddits_controller.rb
+++ app/controllers/reddits_controller.rb
@@ -6,4 +6,47 @@ class RedditsController < ApplicationController
   def new
     @reddit = Reddit.new
   end
+
+  def create
+    @reddit = Reddit.new(reddit_params)
+
+    if @reddit.save
+      redirect_to reddits_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @reddit = Reddit.find(params[:id])
+  end
+
+  def edit
+    @reddit = Reddit.find(params[:id])
+  end
+
+  def update
+    @reddit = Reddit.find(params[:id])
+
+    if @reddit.update(reddit_params)
+      redirect_to reddits_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @reddit = Reddit.find(params[:id])
+    @reddit.destroy
+    redirect_to reddits_path
+  end
+
+  private
+
+  def reddit_params
+    params.require(:reddit).permit(:title, :url)
+  end
+
+
+
 end
