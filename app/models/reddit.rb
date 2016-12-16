c4a7b835c6e7c0ff0285d71aae4e45cb6e76cf13
rorusage <rorusage@gmail.com>
2016-12-16 16:48:55 +0800

    Only the author can see the edit and delete button

--- app/models/reddit.rb
+++ app/models/reddit.rb
@@ -2,4 +2,8 @@ class Reddit < ActiveRecord::Base
   validates :title, presence: true
   validates :url, presence: true
   belongs_to :author, class_name: "User", foreign_key: :user_id
+
+  def editable_by?(user)
+    user == author
+  end
 end
