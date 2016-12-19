6a3d47eca8b94f42a0449f8dbe6f16c35020bfd0
rorusage <rorusage@gmail.com>
2016-12-19 16:39:32 +0800

    Create comment model and controller, build the relation with reddit

--- app/models/reddit.rb
+++ app/models/reddit.rb
@@ -2,6 +2,7 @@ class Reddit < ActiveRecord::Base
   validates :title, presence: true
   validates :url, presence: true
   belongs_to :author, class_name: "User", foreign_key: :user_id
+  has_many :comments
 
   def editable_by?(user)
     user == author
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161216082330) do
+ActiveRecord::Schema.define(version: 20161219082957) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "reddit_id"
+    t.string   "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reddits", force: :cascade do |t|
     t.string   "title"
